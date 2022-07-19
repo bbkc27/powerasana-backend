db579592b529abf2dfee0a734c4e6b79dd775eb1
bbkc27 <briana.j.casey@gmail.com>
2022-07-19 18:48:33 -0400

    Attempt to connect front and back deployed 2

--- powerasana_django/settings.py
+++ powerasana_django/settings.py
@@ -60,11 +60,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ORIGINS = [
-    "https://powerasana.netlify.app/",
-    "http://localhost:3000/",
-    "http://127.0.0.1:3000/"
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'powerasana_django.urls'
 

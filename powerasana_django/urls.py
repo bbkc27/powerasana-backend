b196f923cbf730781829aa4736e0eef402c5cf5c
bbkc27 <briana.j.casey@gmail.com>
2022-07-20 19:54:38 -0400

    Debugging create

--- powerasana_django/urls.py
+++ powerasana_django/urls.py
@@ -26,5 +26,4 @@ urlpatterns = [
     path('', include('powerasana.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
 ]
